7c90f28bfd3958601a1a8eb47a4a20f2851407bd
Dee Race <deerace12@gmail.com>
2018-08-08 14:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function justInvoke(fn) {
   return fn()
 };
 
-function setThisWithCall(fn, thisValue, arg) { 
+function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, [args])
 }
